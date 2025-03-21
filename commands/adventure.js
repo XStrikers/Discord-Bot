a3d0d17f844d7d863545722f7eae6c3f7448004b
XStrikers <info@xstrikers.de>
2025-03-21 23:42:32 +0100

    Update adventure.js

--- commands/adventure.js
+++ commands/adventure.js
@@ -12,7 +12,7 @@ export default {
         const currentDate = new Date();
 
         try {
-            const [rows] = await pool.execute('SELECT discord_id FROM discord_user WHERE discord_id = ?', [userId]);
+            const [rows] = await pool.execute('SELECT * FROM discord_user WHERE discord_id = ?', [userId]);
 
             if (rows.length === 0) {
                 const embed = new EmbedBuilder()
@@ -22,12 +22,8 @@ export default {
                 return interaction.reply({ embeds: [embed] });
             }
 
-            const [workRows] = await pool.execute('SELECT last_work, coins, current_xp, level, xp_needed FROM discord_user WHERE discord_id = ?', [userId]);
-            let lastWork = workRows.length > 0 && workRows[0].last_work ? new Date(workRows[0].last_work) : null;
-            let userCoins = workRows.length > 0 ? workRows[0].coins : 0;
-            let currentXP = workRows[0].current_xp;
-            let level = workRows[0].level;
-            let xpNeeded = workRows[0].xp_needed;
+            const { last_work, coins: userCoins, current_xp, level, xp_needed } = rows[0];
+            let lastWork = last_work ? new Date(last_work) : null;
 
             if (lastWork) {
                 const timeDiff = (currentDate - lastWork) / 1000;
@@ -35,30 +31,33 @@ export default {
                     const earnedCoins = Math.floor(Math.random() * 51) + 50;
                     const earnedXP = Math.floor(Math.random() * 51) + 50;
                     const newCoins = userCoins + earnedCoins;
-                    currentXP += earnedXP;
+                    let currentXP = current_xp + earnedXP;
 
-                    while (currentXP >= xpNeeded) {
-                    level++;
-                        let overflowXP = currentXP - xpNeeded;
-                        xpNeeded = Math.floor(xpNeeded * 1.5);
+                    let newLevel = level;
+                    while (currentXP >= xp_needed) {
+                        newLevel++;
+                        let overflowXP = currentXP - xp_needed;
+                        xp_needed = Math.floor(xp_needed * 1.5);
                         currentXP = overflowXP;
                     }
 
-                    await pool.execute('UPDATE discord_user SET coins = ?, current_xp = ?, level = ?, xp_needed = ?, last_work = ? WHERE discord_id = ?', [newCoins, currentXP, level, xpNeeded, currentDate, userId]);
+                    await pool.execute('UPDATE discord_user SET coins = ?, current_xp = ?, level = ?, xp_needed = ?, last_work = ? WHERE discord_id = ?', 
+                        [newCoins, currentXP, newLevel, xp_needed, currentDate, userId]);
 
                     const embed = new EmbedBuilder()
                         .setTitle('🧭 Neues Abenteuer gestartet!')
                         .setDescription(`Du hast dein letztes Abenteuer abgeschlossen und **${earnedCoins}** <:xscoins:1346851584985792513> sowie **${earnedXP} XP** erhalten!\nAktuell hast du in deinem XS-Coins Beutel **${newCoins}** <:xscoins:1346851584985792513> gesammelt.\n\nDu hast ein neues Abenteuer begonnen, kehre in **1 Stunde** zurück.`)
                         .setColor(0x26d926);
 
-                    if (level > workRows[0].level) {
+                    if (newLevel > level) {
                         const channel = await client.channels.fetch(process.env.LEVEL_UP_CHANNEL);
                         const levelUpEmbed = new EmbedBuilder()
                             .setTitle('<:epic:1346851964389953546> Level Up')
-                            .setDescription(`**${interaction.user.displayName}**, du hast das **Level ${level}** erreicht <:epic:1346851964389953546>\nMach weiter so und stürze dich ins nächste Abenteuer.`)
+                            .setDescription(`**${interaction.user.displayName}**, du hast das **Level ${newLevel}** erreicht <:epic:1346851964389953546>\nMach weiter so und stürze dich ins nächste Abenteuer.`)
                             .setColor(0x26d926);
                         await channel.send({ embeds: [levelUpEmbed] });
                     }
+
                     return interaction.reply({ embeds: [embed] });
                 } else {
                     const remainingTime = Math.ceil((3600 - timeDiff) / 60);
@@ -66,7 +65,7 @@ export default {
                         .setTitle('🧭 Aktives Abenteuer')
                         .setDescription(`Du bist noch auf einem Abenteuer! \nIn **${remainingTime} Minuten** hast du dein Abenteuer beendet und kannst dann deine <:xscoins:1346851584985792513> zur Belohnung abholen.`)
                         .setColor(0xd98226);
-                        return interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
+                    return interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
                 }
             }
 
@@ -86,7 +85,7 @@ export default {
                 .setDescription(`Es gab ein Problem beim Starten deines Abenteuers. Versuche es bitte erneut.`)
                 .setColor(0xd92626);
 
-            return interaction.reply({ embeds: [embed], flags: 64 });
+            return interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
         }
     }
-}
+};
