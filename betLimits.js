56f9588988953cedaa5a9a1f5dde7d6eae9fbe80
XStrikers <info@xstrikers.de>
2025-03-27 14:43:42 +0100

    Create betLimits.js

---
+++ betLimits.js
@@ -0,0 +1,8 @@
+export const betLimits = {
+    dice: {
+        maxBet: 1000,  // Maximaler Einsatz für das Dice-Spiel
+    },
+    roulette: {
+        maxBet: 5000,  // Maximaler Einsatz für das Roulette-Spiel
+    },
+};
