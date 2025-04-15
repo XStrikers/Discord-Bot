dcb4aaf9a798160025607d36e02aeaa32004fac3
XStrikers <info@xstrikers.de>
2025-04-15 14:41:16 +0200

    Update farming.js

--- commands/farming.js
+++ commands/farming.js
@@ -170,7 +170,7 @@ export default {
                     const plant = plants[plant_type];
                     const timeDiff = (currentDate - new Date(plant_time)) / 60000;
                     statusMessage = timeDiff < plant.cooldown
-                        ? `**${plant_type}** wächst noch. Ernte in **${Math.ceil(plant.cooldown - timeDiff)} Minuten**.`
+                        ? `**${plant_type}** wächst noch und kann erst in **${Math.ceil(plant.cooldown - timeDiff)} Minuten** geerntet werden..`
                         : `**${plant_type} ist bereit zur Ernte!**`;
                 } else {
                     statusMessage ='🌱 Du hast momentan nichts gepflanzt.'
@@ -179,7 +179,7 @@ export default {
                 return interaction.reply({
                     embeds: [new EmbedBuilder()
                         .setTitle('🌾 Dein Farming-Status')
-                        .setDescription(`**Level:** ${level}\n**XP:** ${current_xp}/${xp_needed}\n\n${statusMessage}`)
+                        .setDescription(`**Dein aktuelles LEvel ist **${level}\nAktuell hat Du auf deiner Farm **${current_xp} / ${xp_needed} XP** gesammelt.\n\n${statusMessage}`)
                         .setColor(0x26d926)]
                 });
             }
