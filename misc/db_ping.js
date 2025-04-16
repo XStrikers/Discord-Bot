4ac5e2dab7eaaf9ae869a766c5d95492fc2a1f17
XStrikers <info@xstrikers.de>
2025-04-16 11:04:01 +0200

    Update db_ping.js

--- misc/db_ping.js
+++ misc/db_ping.js
@@ -1,13 +1,30 @@
 // db_ping.js
 import { pool } from '../economy.js';
 
-export const startDbPing = () => {
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+export const startDbPing = (client) => {
     setInterval(async () => {
         try {
             await pool.query('SELECT 1');
             console.log('🔄 Datenbank-Ping erfolgreich');
+
+            // Nachricht im Discord-Channel senden
+            const channel = await client.channels.fetch(CHANNEL_ID);
+            if (channel) {
+                await channel.send('✅ Datenbank-Ping erfolgreich ausgeführt!');
+                console.log("✅ PIng an die Datenbank gesendet");
+            } else {
+                console.warn('⚠️ Ping-Channel nicht gefunden.');
+            }
         } catch (error) {
             console.error('❌ Datenbank-Ping fehlgeschlagen:', error);
+
+            // Fehler im Channel melden
+            const channel = await client.channels.fetch(CHANNEL_ID);
+            if (channel) {
+                channel.send('❌ Fehler beim Datenbank-Ping! Details in der Konsole.');
+            }
         }
-    }, 10 * 60 * 1000);
+    }, 10 * 60 * 1000); // alle 10 Minuten
 };
