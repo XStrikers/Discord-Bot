67202eaee0f736b2d655d1ba5d2cac42d04f257d
XStrikers <info@xstrikers.de>
2025-06-10 21:25:35 +0200

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -24,7 +24,6 @@ export default {
                     .setTitle(':x: Game nicht vorhanden')
                     .setDescription(`Das Minigame **Dice** befindet sich im Channel <#1382047851512856667> und kann nur noch dort gespielt werden.`)
                     .setColor(0xd92626)
-                    .setImage('https://xstrikers.de/discord/images/truck_loading.png')
                 ],
                 flags: 64
             });
