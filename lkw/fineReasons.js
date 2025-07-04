7e0943aaa6701eab59e97bc4c2508f7e048d58fa
XStrikers <info@xstrikers.de>
2025-07-04 19:35:56 +0200

    Update fineReasons.js

--- lkw/fineReasons.js
+++ lkw/fineReasons.js
@@ -1,10 +1,10 @@
 export const fineReasons = [
-  { reason: "Erhöhtes Tempo auf der Autobahn", amount: 320, image: "https://xstrikers.de/discord/images/police_highway.png" },
-  { reason: "Erhöhtes Tempo in der Stadt", amount: 270, image: "https://xstrikers.de/discord/images/police_city.png" },
-  { reason: "Rote Ampel überfahren", amount: 350, image: "https://xstrikers.de/discord/images/police_red_light.png" },
-  { reason: "Erhöhtes Tempo bei einer Baustelle", amount: 300, image: "https://xstrikers.de/discord/images/police_construction.png" },
-  { reason: "Einen Autounfall verursacht", amount: 500, image: "https://xstrikers.de/discord/images/police_car.png" },
-  { reason: "Verkehrskontrolle (Autoteile defekt)", amount: 400, image: "https://xstrikers.de/discord/images/police_checked.png" },
-  { reason: "Alkohol beim Fahren", amount: 600, image: "https://xstrikers.de/discord/images/police_alcoholic.png" },
-  { reason: "Drogenkontrolle", amount: 800, image: "https://xstrikers.de/discord/images/police_drugs.png" }
+  { reason: "Erhöhtes Tempo auf der Autobahn", amount: 120, image: "https://xstrikers.de/discord/images/police_highway.png" },
+  { reason: "Erhöhtes Tempo in der Stadt", amount: 70, image: "https://xstrikers.de/discord/images/police_city.png" },
+  { reason: "Rote Ampel überfahren", amount: 150, image: "https://xstrikers.de/discord/images/police_red_light.png" },
+  { reason: "Erhöhtes Tempo bei einer Baustelle", amount: 100, image: "https://xstrikers.de/discord/images/police_construction.png" },
+  { reason: "Einen Autounfall verursacht", amount: 300, image: "https://xstrikers.de/discord/images/police_car.png" },
+  { reason: "Verkehrskontrolle (Autoteile defekt)", amount: 200, image: "https://xstrikers.de/discord/images/police_checked.png" },
+  { reason: "Alkohol beim Fahren", amount: 400, image: "https://xstrikers.de/discord/images/police_alcoholic.png" },
+  { reason: "Drogenkontrolle", amount: 600, image: "https://xstrikers.de/discord/images/police_drugs.png" }
 ];
