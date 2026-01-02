5591f34006174bf002c7298cf162530f8ddbcc3d
XStrikers <info@xstrikers.de>
2026-01-02 02:16:28 +0100

    Update livestatus.db.js

--- twitch/livestatus.db.js
+++ twitch/livestatus.db.js
@@ -1,8 +1,8 @@
-import db from '../misc/database.js';
+import db from '../economy.js';
 
 export async function getLiveStatus(streamer) {
     const [rows] = await db.query(
-        'SELECT * FROM twitch_livestatus WHERE streamer = ?',
+        'SELECT * FROM twitch_live_status WHERE streamer = ?',
         [streamer]
     );
     return rows[0] || null;
@@ -10,15 +10,15 @@ export async function getLiveStatus(streamer) {
 
 export async function upsertLiveStatus(data) {
     await db.query(`
-        INSERT INTO twitch_livestatus
+        INSERT INTO twitch_live_status
         (streamer, isLive, announced, messageId, title, viewers)
         VALUES (?, ?, ?, ?, ?, ?)
         ON DUPLICATE KEY UPDATE
-            isLive = VALUES(isLive),
-            announced = VALUES(announced),
-            messageId = VALUES(messageId),
-            title = VALUES(title),
-            viewers = VALUES(viewers)
+        isLive = VALUES(isLive),
+        announced = VALUES(announced),
+        messageId = VALUES(messageId),
+        title = VALUES(title),
+        viewers = VALUES(viewers)
     `, [
         data.streamer,
         data.isLive,
